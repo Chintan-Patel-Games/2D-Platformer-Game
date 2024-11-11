85facbffadb7c5b7c0ad9ef95615512a166ebd7b
Chintan Patel <chintanpatelit024@gmqail.com>
2024-11-11 11:00:46 +0530

    Added Player crouch logic

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -10,19 +7,37 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        PlayerMoveControls();
+        PlayerCrouchControl();
+    }
+
+    private void PlayerMoveControls()
+    {
         float speed = Input.GetAxisRaw("Horizontal");
         animator.SetFloat("Speed", Mathf.Abs(speed));
 
         Vector3 scale = transform.localScale;
 
-        if(speed < 0)
+        if (speed < 0)
         {
             scale.x = -1f * Mathf.Abs(scale.x);
         }
-        else if(speed > 0)
+        else if (speed > 0)
         {
             scale.x = Mathf.Abs(scale.x);
         }
         transform.localScale = scale;
+    }
+
+    private void PlayerCrouchControl()
+    {
+        if (Input.GetKey(KeyCode.C))
+        {
+            animator.SetBool("Crouch", true);
+        }
+        else
+        {
+            animator.SetBool("Crouch", false);
+        }
     }
 }
